bdf6fe2e13f9702d0f44d3a4233c46e2367ea7a8
asimcanuz <asimcanuz@gmail.com>
2025-02-24 01:29:43 +0300

    refactor: remove unused imports and unnecessary CommandLineRunner from WarehousemanagementApplication

--- src/main/java/org/asodev/monolithic/warehousemanagement/WarehousemanagementApplication.java
+++ src/main/java/org/asodev/monolithic/warehousemanagement/WarehousemanagementApplication.java
@@ -1,36 +1,14 @@
 package org.asodev.monolithic.warehousemanagement;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.jdbc.core.JdbcTemplate;
-
 
 @SpringBootApplication
 public class WarehousemanagementApplication {
-	private static final Logger log = LoggerFactory.getLogger(WarehousemanagementApplication.class);
-
-	private final WarehouseService warehouseService;
-
-	public WarehousemanagementApplication(WarehouseService warehouseService) {
-		this.warehouseService = warehouseService;
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(WarehousemanagementApplication.class, args);
 	}
 
-	@Bean
-	public CommandLineRunner createData(JdbcTemplate jdbcTemplate) {
-		return args -> {
-			log.info("Starting data creation...");
-			warehouseService.createCategories(jdbcTemplate);
-			warehouseService.createProducts(jdbcTemplate);
-			log.info("Data creation completed.");
-		};
-	}
 }
 
